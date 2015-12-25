f325d18ddf7bb7b8089608fd86cd167db40d0c75
诸谦 <zq317157782@163.com>
2015-12-25 18:19:48 +0800

    增加Light基类

---
+++ light/Light.h
@@ -0,0 +1,25 @@
+//
+// Created by 诸谦 on 15/12/25.
+//
+
+#ifndef RAYTRACER_LIGHT_H
+#define RAYTRACER_LIGHT_H
+
+
+#include "../base/Vector3.h"
+#include "../base/RGB.h"
+#include "../ShadeRec.h"
+
+/**
+ * 光 基类
+ */
+class Light {
+private:
+    bool mShadow;//是否投射阴影
+public:
+    virtual Vector3 getDirection(const ShadeRec& sr)=0; //得到光线方向
+    virtual RGB L(const ShadeRec& sr)=0;  //得到hit点从这个光源得到的能量
+};
+
+
+#endif //RAYTRACER_LIGHT_H
