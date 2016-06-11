013f15a916e636bd6436fbfe5f9f25f39d4c4f07
诸谦 <zq317157782@163.com>
2016-06-11 12:57:30 +0800

    加入memory.h/cpp

---
+++ core/memory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 诸谦 on 16/6/11.
+//
+
+#include "memory.h"
---
+++ core/memory.h
@@ -0,0 +1,43 @@
+//
+// Created by 诸谦 on 16/6/11.
+//
+
+#ifndef RAYTRACER_MEMORY_H
+#define RAYTRACER_MEMORY_H
+
+template <typename T> class Reference {
+public:
+    // Reference Public Methods
+    Reference(T *p = nullptr) {
+        ptr = p;
+        if (ptr) ptr->nReferences+=1;
+    }
+    Reference(const Reference<T> &r) {
+        ptr = r.ptr;
+        if (ptr)ptr->nReferences+=1;
+    }
+    Reference &operator=(const Reference<T> &r) {
+        if (r.ptr) r.ptr->nReferences+=1;
+        if (ptr && (ptr->nReferences-=1) == 0) delete ptr;
+        ptr = r.ptr;
+        return *this;
+    }
+    Reference &operator=(T *p) {
+        if (p) p->nReferences+=1;
+        if (ptr && (ptr->nReferences-=1) == 0) delete ptr;
+        ptr = p;
+        return *this;
+    }
+    ~Reference() {
+        if (ptr &&(ptr->nReferences-=1) == 0)
+            delete ptr;
+    }
+    T *operator->() { return ptr; }
+    const T *operator->() const { return ptr; }
+    operator bool() const { return ptr != nullptr; }
+    const T *GetPtr() const { return ptr; }
+private:
+    T *ptr;
+};
+
+#endif //RAYTRACER_MEMORY_H
