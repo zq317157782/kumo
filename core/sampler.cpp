2664a6754b45f58f698d74d910893ab3d3754bee
诸谦(work) <zq317157782@163.com>
2016-07-01 17:06:10 +0800

    添加新的采样器头文件sampler.h

---
+++ core/sampler.cpp
@@ -0,0 +1,8 @@
+/*
+ * sampler.cpp
+ *
+ *  Created on: 2016年7月1日
+ *      Author: Administrator
+ */
+
+#include <sampler.h>
---
+++ core/sampler.h
@@ -0,0 +1,14 @@
+/*
+ * sampler.h
+ *
+ *  Created on: 2016年7月1日
+ *      Author: Administrator
+ */
+
+#ifndef CORE_SAMPLER_H_
+#define CORE_SAMPLER_H_
+
+
+
+
+#endif /* CORE_SAMPLER_H_ */
