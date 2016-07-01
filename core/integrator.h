41152bcaddc267d5b4f50485f8bbb01c0e00f078
诸谦(work) <zq317157782@163.com>
2016-07-01 15:07:09 +0800

    添加积分器原型

---
+++ core/integrator.h
@@ -0,0 +1,17 @@
+//
+// Created by 诸谦 on 16/7/1.
+//
+
+#ifndef RAYTRACER_INTEGRATOR_H
+#define RAYTRACER_INTEGRATOR_H
+#include "global.h"
+//积分器
+class Integrator{
+
+};
+
+
+class SurfaceIntegrator:public Integrator{
+
+};
+#endif //RAYTRACER_INTEGRATOR_H
