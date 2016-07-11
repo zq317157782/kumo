5d357eb16ff09b0f171ae80464fc010eea2896d8
诸谦(work) <zq317157782@163.com>
2016-07-11 18:24:20 +0800

    添加点光源，但是Sample_L还没有实现

---
+++ light/point.cpp
@@ -0,0 +1,16 @@
+/*
+ * light.cpp
+ *
+ *  Created on: 2016年7月11日
+ *      Author: Administrator
+ */
+#include "point.h"
+
+//todo 还没有实现
+RGB PointLight::Sample_L(const Point &p, float pEpsilon, const LightSample &ls,
+				float time, Vector *wi, float *pdf,
+				VisibilityTester *vis) const{
+
+}
+
+
---
+++ light/point.h
@@ -0,0 +1,30 @@
+/*
+ * point.h
+ *
+ *  Created on: 2016年7月11日
+ *      Author: Administrator
+ */
+
+#ifndef LIGHT_POINT_H_
+#define LIGHT_POINT_H_
+
+#include "global.h"
+#include "light.h"
+#include "RGB.h"
+class PointLight:public Light{
+public:
+	PointLight(const Transform& l2w,const RGB& intensity):Light(l2w){
+		mPos=lightToWorld(Point(0,0,0));
+		mIntensity=intensity;
+	};
+
+	virtual RGB Sample_L(const Point &p, float pEpsilon, const LightSample &ls,
+				float time, Vector *wi, float *pdf,
+				VisibilityTester *vis) const override;
+private:
+	RGB mIntensity;//光源强度
+	Point mPos;
+};
+
+
+#endif /* LIGHT_POINT_H_ */
