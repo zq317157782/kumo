b54ee323f141b1217b1dfff30f94c0cc1a024607
诸谦(work) <zq317157782@163.com>
2016-07-19 14:15:09 +0800

    为metal材质添加 反射坐标系转换的  代码

--- material/metal.cpp
+++ material/metal.cpp
@@ -9,33 +9,42 @@
 #include <Scene.h>
 #include "reflection.h"
 
-Metal::Metal(const RGB& reflectance,const RGB& e, const RGB& kk,MicrofacetDistribution* dis){
-	mFresnel=new FresnelConductor(e,kk);
-	mBrdf=new Microfacet(reflectance,mFresnel,dis);
+Metal::Metal(const RGB& reflectance, const RGB& e, const RGB& kk,
+		MicrofacetDistribution* dis) {
+	mFresnel = new FresnelConductor(e, kk);
+	mBrdf = new Microfacet(reflectance, mFresnel, dis);
 }
 
+RGB Metal::shade(const Intersection &sr) {
 
-RGB Metal::shade(const Intersection &sr){
-	Vector wo=-sr.ray.d;
-    Point hitPoint=sr.ray(sr.distance);
-    Vector wi;
-    float pdf;
-    VisibilityTester vt;
-    RGB L(0,0,0);
-    LightSample ls;
-    unsigned long lignt_num=sr.scene.getLightNum();
-    for(unsigned long i=0;i<lignt_num;++i){
-        RGB l=sr.scene.getLight(i)->Sample_L(hitPoint,0,ls,&wi,&pdf,&vt);
-        if(!vt.Unoccluded(&sr.scene)) continue;
-        double ndotl=Dot(sr.normal,wi);
-        if(ndotl>0){
-        	//cout<<"brdf:"<<mBrdf->f(wi,wo).r<<"brdf:"<<mBrdf->f(wi,wo).g<<"brdf:"<<mBrdf->f(wi,wo).b<<endl;
-            L=L+l*mBrdf->f(wi,wo)*ndotl;  //mDiffuseBrdf.f(sr,wi,wo)
-        }
-    }
-    return L;
+	Vector nn = sr.normal;
+	Vector sn = Normalize(sr.dg.dpdu);
+	Vector tn = Cross(nn, sn);
+
+	Vector wo = -sr.ray.d;
+	Point hitPoint = sr.ray(sr.distance);
+	Vector wi;
+	float pdf;
+	VisibilityTester vt;
+	RGB L(0, 0, 0);
+	LightSample ls;
+	unsigned long lignt_num = sr.scene.getLightNum();
+	for (unsigned long i = 0; i < lignt_num; ++i) {
+		RGB l = sr.scene.getLight(i)->Sample_L(hitPoint, 0, ls, &wi, &pdf, &vt);
+		if (!vt.Unoccluded(&sr.scene))
+			continue;
+		double ndotl = Dot(sr.normal, wi);
+		if (ndotl > 0) {
+			wo = Vector(Dot(sn, wo), Dot(tn, wo), Dot(nn, wo));//转换到反射坐标系下
+			wi = Vector(Dot(sn, wi), Dot(tn, wi), Dot(nn, wi));
+
+			//cout<<"brdf:"<<mBrdf->f(wi,wo).r<<"brdf:"<<mBrdf->f(wi,wo).g<<"brdf:"<<mBrdf->f(wi,wo).b<<endl;
+			L = L + l * mBrdf->f(wi, wo) * ndotl;  //mDiffuseBrdf.f(sr,wi,wo)
+		}
+	}
+	return L;
 }
 
 RGB Metal::areaLightShade(Intersection &sr) {
-    return RGB(0,0,0);
+	return RGB(0, 0, 0);
 }
