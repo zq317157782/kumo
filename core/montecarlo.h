03d30fe955f7b8f51b68966fb2cb2891bb35697b
诸谦(work) <zq317157782@163.com>
2016-07-27 17:28:57 +0800

    添加使用拒绝采样方式采样圆盘的方法

--- core/montecarlo.h
+++ core/montecarlo.h
@@ -12,7 +12,7 @@
 #include "global.h"
 
 //均匀的1维分布 作用域x为0~1
-struct Distribution1D{
+struct Distribution1D {
 private:
 	int mCount;
 	float* mFunc;
@@ -23,60 +23,70 @@ public:
 	 *data 数据数组
 	 *n    数据个数
 	 * */
-	Distribution1D(float* data,int n){
-		mCount=n;
-		mFunc=new float[mCount];
-		memcpy(mFunc,data,sizeof(float)*mCount);
-		mCDF=new float[mCount+1];
+	Distribution1D(float* data, int n) {
+		mCount = n;
+		mFunc = new float[mCount];
+		memcpy(mFunc, data, sizeof(float) * mCount);
+		mCDF = new float[mCount + 1];
 
 		//这里面一开始放的是相应积分域上的积分值
-		mCDF[0]=0;
-		for(int i=1;i<mCount+1;++i){
-			mCDF=mFunc[i-1]/mCount+mCDF[i-1]; //注意，这里是积分值，还没有标准化
+		mCDF[0] = 0;
+		for (int i = 1; i < mCount + 1; ++i) {
+			mCDF = mFunc[i - 1] / mCount + mCDF[i - 1]; //注意，这里是积分值，还没有标准化
 		}
 
-		mFuncInt=mCDF[mCount];//作用域上的总的积分值
+		mFuncInt = mCDF[mCount]; //作用域上的总的积分值
 
 		//下面是标准化操作 使得CDF下面存放的是累积概率
-		if(mFuncInt==0){
-			for(int i=0;i<mCount+1;++i)
-				mCDF[i]=float(i)/float(mCount);
-		}
-		else{
-			for(int i=0;i<mCount+1;++i)
-				mCDF[i]/=mFuncInt;
+		if (mFuncInt == 0) {
+			for (int i = 0; i < mCount + 1; ++i)
+				mCDF[i] = float(i) / float(mCount);
+		} else {
+			for (int i = 0; i < mCount + 1; ++i)
+				mCDF[i] /= mFuncInt;
 		}
 
 	}
 
-	~Distribution1D(){
-		delete []mFunc;
-		delete []mCDF;
+	~Distribution1D() {
+		delete[] mFunc;
+		delete[] mCDF;
 	}
 
 	/*
 	 * u 0~1之间的   pdf概率  off偏移
 	 * */
-	float SampleContinuous(float u,float* pdf,int *off) const{
-		 float *ptr=std::upper_bound(mCDF,mCDF+mCount+1,u);
-		 int offset= max(0,int(ptr-mCDF-1)); //offset是小于u的cdf所在的偏移
-		 if(off) *off=offset;
-		 float du=(u-mCDF[offset])/(mCDF[offset+1]-mCDF[offset]);//u在cdf[offset+1]-cdf[offset]之间的所在位置百分比
+	float SampleContinuous(float u, float* pdf, int *off) const {
+		float *ptr = std::upper_bound(mCDF, mCDF + mCount + 1, u);
+		int offset = max(0, int(ptr - mCDF - 1)); //offset是小于u的cdf所在的偏移
+		if (off)
+			*off = offset;
+		float du=(u-mCDF[offset])/(mCDF[offset+1]W-mCDF[offset]); //u在cdf[offset+1]-cdf[offset]之间的所在位置百分比
 
-		 if(pdf) *pdf=mCDF[offset]/mFuncInt;//计算offset下的概率密度
-		 return (offset+du)/mCount;//计算连续的返回值，在0~1之间
+		if (pdf)
+			*pdf = mCDF[offset] / mFuncInt; //计算offset下的概率密度
+		return (offset + du) / mCount; //计算连续的返回值，在0~1之间
 	}
 
 	//采样离散版本
-	int SampleDiscrete(float u,float* pdf){
-		 float *ptr=std::upper_bound(mCDF,mCDF+mCount+1,u);
-		 int offset= max(0,int(ptr-mCDF-1)); //offset是小于u的cdf所在的偏移
-		 if(pdf) *pdf=mCDF[offset]/mFuncInt;//计算offset下的概率密度
-		 return offset;
+	int SampleDiscrete(float u, float* pdf) {
+		float *ptr = std::upper_bound(mCDF, mCDF + mCount + 1, u);
+		int offset = max(0, int(ptr - mCDF - 1)); //offset是小于u的cdf所在的偏移
+		if (pdf)
+			*pdf = mCDF[offset] / mFuncInt; //计算offset下的概率密度
+		return offset;
 	}
 };
 
-
-
+//使用拒绝采样方法来采样圆盘
+void RejectionSampleDisk(float* x, float*y, Random& rand) {
+	float sx, sy;
+	do {
+		sx = 1 - 2 * rand.RandomFloat();
+		sy = 1 - 2 * rand.RandomFloat();
+	} while (sx * sx + sy * sy > 1f);
+	*x=sx;
+	*y=sy;
+}
 
 #endif /* CORE_MONTECARLO_H_ */
