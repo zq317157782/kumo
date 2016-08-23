e6a53f9af989164b1c27d81d587bfabe65aaa0f7
诸谦 <zq317157782@163.com>
2016-08-23 16:17:57 +0800

    提交玻璃材质

---
+++ src/material/grass.cpp
@@ -0,0 +1,26 @@
+/*
+ * grass.cpp
+ *
+ *  Created on: 2016年8月23日
+ *      Author: zhuqian
+ */
+#include "grass.h"
+#include "reflection.h"
+#include "texture.h"
+
+BSDF * Grass::GetBSDF(const DifferentialGeometry &dgGeom,
+				const DifferentialGeometry &dgShading, MemoryArena &arena) const{
+	 DifferentialGeometry dgs;
+	 dgs = dgShading;
+	 float ior = mIndex->Evaluate(dgs);//材质折射率
+	 BSDF *bsdf = BSDF_ALLOC(arena, BSDF)(dgs, dgGeom.nn, ior);
+	 RGB R = mKr->Evaluate(dgs).clamp();
+	 RGB T = mKt->Evaluate(dgs).clamp();
+	 if(!R.IsBlack()){
+		 bsdf->Add(BSDF_ALLOC(arena, SpecularReflection)(R,BSDF_ALLOC(arena, FresnelDielectric)(1.0f, ior)));//镜面反射BRDF
+	 }
+	 if(!T.IsBlack()){
+		 bsdf->Add(BSDF_ALLOC(arena, SpecularTransmission)(T, 1.0f, ior));
+	 }
+	 return bsdf;
+}
---
+++ src/material/grass.h
@@ -0,0 +1,31 @@
+/*
+ * grass.h
+ *
+ *  Created on: 2016年8月23日
+ *      Author: zhuqian
+ */
+
+#ifndef SRC_MATERIAL_GRASS_H_
+#define SRC_MATERIAL_GRASS_H_
+#include "global.h"
+#include "material.h"
+//玻璃材质
+class Grass:public Material{
+private:
+	 Reference<Texture<RGB> > mKr, mKt;
+	 Reference<Texture<float> > mIndex;
+public:
+	Grass(Reference<Texture<RGB> > r, Reference<Texture<RGB> > t,
+            Reference<Texture<float> > i){
+		mKr=r;
+		mKt=t;
+		mIndex=i;
+	}
+	virtual ~Grass(){}
+	virtual BSDF *GetBSDF(const DifferentialGeometry &dgGeom,
+				const DifferentialGeometry &dgShading, MemoryArena &arena) const
+						override;
+};
+
+
+#endif /* SRC_MATERIAL_GRASS_H_ */
