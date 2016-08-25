126ac41ba780add942fa9fb442bd90000c061a7e
诸谦 <zq317157782@163.com>
2016-08-25 13:26:42 +0800

    添加PBRT中的BlockArray实现

--- src/core/memory.h
+++ src/core/memory.h
@@ -116,5 +116,59 @@ private:
     vector<char *> usedBlocks, availableBlocks;
 };
 
+//直接从PBRT复制，是一个有效避免Cacha Miss的数组
+template <typename T, int logBlockSize> class BlockedArray {
+public:
+    // BlockedArray Public Methods
+    BlockedArray(uint32_t nu, uint32_t nv, const T *d = NULL) {
+        uRes = nu;
+        vRes = nv;
+        uBlocks = RoundUp(uRes) >> logBlockSize;
+        uint32_t nAlloc = RoundUp(uRes) * RoundUp(vRes);
+        data = AllocAligned<T>(nAlloc);
+        for (uint32_t i = 0; i < nAlloc; ++i)
+            new (&data[i]) T();
+        if (d)
+            for (uint32_t v = 0; v < vRes; ++v)
+                for (uint32_t u = 0; u < uRes; ++u)
+                    (*this)(u, v) = d[v * uRes + u];
+    }
+    uint32_t BlockSize() const { return 1 << logBlockSize; }
+    uint32_t RoundUp(uint32_t x) const {
+        return (x + BlockSize() - 1) & ~(BlockSize() - 1);
+    }
+    uint32_t uSize() const { return uRes; }
+    uint32_t vSize() const { return vRes; }
+    ~BlockedArray() {
+        for (uint32_t i = 0; i < uRes * vRes; ++i)
+            data[i].~T();
+        FreeAligned(data);
+    }
+    uint32_t Block(uint32_t a) const { return a >> logBlockSize; }
+    uint32_t Offset(uint32_t a) const { return (a & (BlockSize() - 1)); }
+    T &operator()(uint32_t u, uint32_t v) {
+        uint32_t bu = Block(u), bv = Block(v);
+        uint32_t ou = Offset(u), ov = Offset(v);
+        uint32_t offset = BlockSize() * BlockSize() * (uBlocks * bv + bu);
+        offset += BlockSize() * ov + ou;
+        return data[offset];
+    }
+    const T &operator()(uint32_t u, uint32_t v) const {
+        uint32_t bu = Block(u), bv = Block(v);
+        uint32_t ou = Offset(u), ov = Offset(v);
+        uint32_t offset = BlockSize() * BlockSize() * (uBlocks * bv + bu);
+        offset += BlockSize() * ov + ou;
+        return data[offset];
+    }
+    void GetLinearArray(T *a) const {
+        for (uint32_t v = 0; v < vRes; ++v)
+            for (uint32_t u = 0; u < uRes; ++u)
+                *a++ = (*this)(u, v);
+    }
+private:
+    // BlockedArray Private Data
+    T *data;
+    uint32_t uRes, vRes, uBlocks;
+};
 
 #endif //RAYTRACER_MEMORY_H
