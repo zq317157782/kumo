1decee4531280f4a8a26640818d63a59b7b9ad75
诸谦 <zq317157782@163.com>
2016-10-10 11:45:41 +0800

    加入MLT Renderer的头文件和CPP文件，开始实现MLT算法

---
+++ src/renderer/metropolis.cpp
@@ -0,0 +1,10 @@
+/*
+ * metropolis.cpp
+ *
+ *  Created on: 2016年10月10日
+ *      Author: zhuqian
+ */
+#include "metropolis.h"
+
+
+
---
+++ src/renderer/metropolis.h
@@ -0,0 +1,18 @@
+/*
+ * metropolis.h
+ *
+ *  Created on: 2016年10月10日
+ *      Author: zhuqian
+ */
+
+#ifndef SRC_RENDERER_METROPOLIS_H_
+#define SRC_RENDERER_METROPOLIS_H_
+#include "global.h"
+#include "renderer.h"
+
+//MLT算法的实现
+
+
+
+
+#endif /* SRC_RENDERER_METROPOLIS_H_ */
